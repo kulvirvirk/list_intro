933107855032f966b351fd48f465c5931a3ca356
kulvirvirk <kvirkdev@gmail.com>
2020-02-14 19:25:05 +0000

    added example of accessing list element with negative index

--- main.py
+++ main.py
@@ -2,9 +2,14 @@
 
 # 1.create & print a list of fruits
 # 2.Access list elements with index number
+# 3.Access last element of list by Negative index
 
+
+# 1.create & print a list of fruits
 my_fav_fruits = ["mango", "kiwi", "banana", "blueberry", "orange"]
+numbers_list = [1,2,3,4,5,2.5]
 print(my_fav_fruits)
+print(numbers_list)
 
 # 2.Access list elements with index number
 # As an ordered sequence of elements, each item in a list can be called individually, through indexing.
@@ -12,3 +17,11 @@ print(my_fav_fruits)
 
 print(my_fav_fruits[2])
 print(my_fav_fruits[4])
+
+print(numbers_list[3])
+
+
+# 3.Access last element of list by Negative index
+print(my_fav_fruits[-1])
+print(numbers_list[-1])
+
