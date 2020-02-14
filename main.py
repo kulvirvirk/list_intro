b727d3fc9cc1bca5bb1cdb2d2d567f4c015a417b
kulvirvirk <kvirkdev@gmail.com>
2020-02-14 19:14:13 +0000

    added code to read elements by index numbers

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 # List is a type of data sturcture, similar to arrays. 
 
 # 1.create & print a list of fruits
+# 2.Access list elements with index number
 
 my_fav_fruits = ["mango", "kiwi", "banana", "blueberry", "orange"]
 print(my_fav_fruits)
+
+# 2.Access list elements with index number
+# As an ordered sequence of elements, each item in a list can be called individually, through indexing.
+# Each item in a list corresponds to an index number, which is an integer value, starting with the index number 0.
+
+print(my_fav_fruits[2])
+print(my_fav_fruits[4])
