4127afa0bfcb2aff715a2f55359980965aab2f53
kulvirvirk <kvirkdev@gmail.com>
2020-02-14 19:09:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# List is a type of data sturcture, similar to arrays. 
+
+# 1.create & print a list of fruits
+
+my_fav_fruits = ["mango", "kiwi", "banana", "blueberry", "orange"]
+print(my_fav_fruits)
