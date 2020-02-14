c2c5d136624a97bb900eaacb288eedd243b30587
kulvirvirk <kvirkdev@gmail.com>
2020-02-14 20:27:54 +0000

    added append method example

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # List is a type of data sturcture, similar to arrays. 
 
-# 1.create & print a list of fruits
-# 2.Access list elements with index number
-# 3.Access last element of list by Negative index
+# 1. create & print a list of fruits
+# 2. Access list elements with index number
+# 3. Access last element of list by Negative index
+# 4. Slice the list to print only element at index 1,2,3
+# 5. try out append method to add element at the end of the list
 
 
 # 1.create & print a list of fruits
@@ -24,4 +26,14 @@ print(numbers_list[3])
 # 3.Access last element of list by Negative index
 print(my_fav_fruits[-1])
 print(numbers_list[-1])
+
+
+# 4. Slice the list to print only element at index 1,2,3 
+print(my_fav_fruits[1:4]) 
+#the first index number is where the slice starts (inclusive), and the second index number is where the slice ends (exclusive), which is why in our example above the items at the position, 1, 2, and 3 are the items that print out.
+
+# 5. try out append method to add element at the end of the list
+my_fav_fruits = ["mango", "kiwi", "banana", "blueberry", "orange"]
+my_fav_fruits.append('watermelon')
+print(my_fav_fruits)
 
